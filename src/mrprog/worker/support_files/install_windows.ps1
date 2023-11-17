f4a4bd9db6e2117a92f1a609dd4b3d9b9452be56
Eric Ahn <wchill1337@gmail.com>
2023-11-17 00:40:52 -0800

    Install virtualenv before creating venv

--- src/mrprog/worker/support_files/install_windows.ps1
+++ src/mrprog/worker/support_files/install_windows.ps1
@@ -5,6 +5,7 @@ git clone https://github.com/wchill/BattleNetworkAutomation
 git clone https://github.com/wchill/BattleNetworkData
 git clone https://github.com/wchill/MrProgUtils
 git clone https://github.com/wchill/MrProgWorker
+python -m pip install virtualenv
 python -m virtualenv venv
 . venv/Scripts/activate.ps1
 
